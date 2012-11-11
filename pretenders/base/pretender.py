7bf52ea74fb50680a171e97c97af655b54d019bc
Becky Lewis <github@scaryclam.co.uk>
2012-11-11 22:07:12 +0000

    Changed the pretenders.py file to be less reliant on python 2.7

--- pretenders/base/pretender.py
+++ pretenders/base/pretender.py
@@ -1,4 +1,7 @@
-import argparse
+try:
+    import argparse
+except ImportError:
+    pass
 import socket
 
 from pretenders.base import (
@@ -43,6 +46,8 @@ class Pretender(object):
 
         Parse command line args and return the parsed object.
         """
+        if not 'argparse' in locals().keys():
+            raise NotImplementedError("Command line not available for you version of python")
         parser = argparse.ArgumentParser(description='Start the server')
         parser.add_argument('-H', '--host', dest='host', default='localhost',
                     help='host/IP to run the server on (default: localhost)')
